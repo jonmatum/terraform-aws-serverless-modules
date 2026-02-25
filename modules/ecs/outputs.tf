268109e7946210103fad849948d8cbc9aae50388
Jonatan Mata <jonmatum@gmail.com>
2026-02-25 17:52:52 -0600

    fix(ecs): add cluster_name output to ECS module

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -3,6 +3,11 @@ output "cluster_id" {
   value       = aws_ecs_cluster.this.id
 }
 
+output "cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.this.name
+}
+
 output "cluster_arn" {
   description = "ARN of the ECS cluster"
   value       = aws_ecs_cluster.this.arn
